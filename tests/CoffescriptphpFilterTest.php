ac4a4393a8c0a7d320c0ad25a74019d3b2c3dbd1
Ben Corlett <bencorlett@me.com>
2013-02-12 12:39:54 +1100

    Adding CoffeeScript PHP compiler.
    Signed-off-by: Ben Corlett <bencorlett@me.com>

---
+++ tests/CoffescriptphpFilterTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Part of the Assetic Filters Package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.  It is also available at
+ * the following URL: http://www.opensource.org/licenses/BSD-3-Clause
+ *
+ * @package    Assetic Filters
+ * @version    2.0
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011 - 2013, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+use Assetic\Asset\FileAsset;
+use Cartalyst\AsseticFilters\CoffeescriptphpFilter;
+
+class CoffeescriptphpFilterTest extends PHPUnit_Framework_TestCase {
+
+	public function testCompilation()
+	{
+		$asset = new FileAsset(__DIR__.'/stubs/coffeescript/script.coffee');
+		$asset->load();
+
+		$filter = new CoffeescriptphpFilter;
+		$filter->filterLoad($asset);
+
+		$expected = file_get_contents(__DIR__.'/stubs/coffeescript/script.js');
+
+		$this->assertEquals($expected, $asset->getContent());
+	}
+
+}
