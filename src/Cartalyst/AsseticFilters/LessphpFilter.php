e5c99ddb48380d76fa59e962e27624f2171ba9d7
Suhayb El Wardany <me@suw.me>
2014-02-05 01:28:24 +0200

    add LessphpFilter
    Signed-off-by: Suhayb El Wardany <me@suw.me>

---
+++ src/Cartalyst/AsseticFilters/LessphpFilter.php
@@ -0,0 +1,59 @@
+<?php namespace Cartalyst\AsseticFilters;
+/**
+ * Part of the Assetic Filters Package.
+ *
+ * NOTICE OF LICENSE
+ *
+ * Licensed under the 3-clause BSD License.
+ *
+ * This source file is subject to the 3-clause BSD License that is
+ * bundled with this package in the LICENSE file.  It is also available at
+ * the following URL: http://www.opensource.org/licenses/BSD-3-Clause
+ *
+ * @package    Assetic Filters
+ * @version    2.0
+ * @author     Cartalyst LLC
+ * @license    BSD License (3-clause)
+ * @copyright  (c) 2011-2014, Cartalyst LLC
+ * @link       http://cartalyst.com
+ */
+
+use Assetic\Asset\AssetInterface;
+use Assetic\Filter\LessphpFilter as AsseticLessphpFilter;
+
+class LessphpFilter extends AsseticLessphpFilter {
+
+	/**
+	 * Filters an asset after it has been loaded.
+	 *
+	 * @param Assetic\Asset\AssetInterface $asset
+	 */
+	public function filterLoad(AssetInterface $asset)
+	{
+		$root = $asset->getSourceRoot();
+		$path = $asset->getSourcePath();
+
+		$dirs = array();
+
+		$lc = new \Less_Parser(array(
+			'compress' => true,
+			'relativeUrls' => false,
+		));
+
+		if ($root && $path)
+		{
+			$dirs[] = dirname($root.'/'.$path);
+		}
+
+		foreach ($this->loadPaths as $loadPath)
+		{
+			$dirs[] = $loadPath;
+		}
+
+		$lc->SetImportDirs($dirs);
+
+		$lc->parseFile($root.'/'.$path, $root);
+
+		$asset->setContent($lc->getCss());
+	}
+}
